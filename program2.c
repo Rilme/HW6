fdda9a13fc4cffd295ebc69f285db3f17b93c887
Rilme <148802621+Rilme@users.noreply.github.com>
2023-11-10 11:42:42 -0600

    program2.c

--- program2.c
+++ program2.c
@@ -1,67 +1,53 @@
- #include <stdio.h>
- #include <stdlib.h>
- #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-  int get_student_grade();
-  int validate_and_get_num_items(const char *input);
+int getStudentGrade();
+void clearInputBuffer();
 
-  int main(int argc, char *argv[]) {
-      if (argc != 2) {
-          printf("Usage: %s <number_of_students>\n", argv[0]);
-          return (-1);
-      }
+int main(int argc, char argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <number_of_students>\n", argv[0]);
+        return 1;
+    }
 
-      int *student_grades;
-      unsigned int num_items = validate_and_get_num_items(argv[1);
+    int numStudents = atoi(argv[1]);
+    if (numStudents <= 0) {
+        printf("Please enter a valid number of students.\n");
+        return 1;
+    }
 
-      if (num_items == 0) {
-          printf("Invalid number of students.\n");
-          return -1;
-      }
+    intstudentGrades = (int )malloc(numStudents sizeof(int));
+    if (studentGrades == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
 
-      student_grades = (int *)malloc(num_items * sizeof(int));
+    for (int i = 0; i < numStudents; i++) {
+        printf("Enter the grade for student %d: ", i + 1);
+        studentGrades[i] = getStudentGrade();
+    }
 
-      if (student_grades == NULL) {
-          printf("Could not allocate memory.\n");
-          return -1;
-      }
+    printf("Student Grades:\n");
+    for (int i = 0; i < numStudents; i++) {
+        printf("Student %d grade: %d\n", i + 1, studentGrades[i]);
+    }
 
-      for (unsigned int ctr = 0; ctr < num_items; ctr++) {
-          printf("\nPlease input student %u's grade: ", ctr);
-          student_grades[ctr] = get_student_grade();
-      }
+    free(studentGrades);
+    return 0;
+}
 
-      for (unsigned int ctr = 0; ctr < num_items; ctr++) {
-          printf("Student %u grade: %d.\n", ctr, student_grades[ctr]);
-      }
+int getStudentGrade() {
+    int grade;
+    if (scanf("%d", &grade) != 1) {
+        printf("Invalid input. Please enter a valid integer.\n");
+        clearInputBuffer();
+        return getStudentGrade();
+    }
+    clearInputBuffer(); // Clear any additional characters in the input buffer.
+    return grade;
+}
 
-      free(student_grades);
-      return 0;
-  }
-
-  int validate_and_get_num_items(const char *input) {
-      int num_items = atoi(input);
-
-      if (num_items <= 0) {
-          return 0; // Invalid input
-      }
-
-      return num_items;
-  }
-
-  int get_student_grade() {
-      int grade;
-      int result = scanf("%d", &grade);
-
-      if (result == EOF) {
-          return 0; // EOF reached
-      }
-
-      if (result == 0) {
-          while (fgetc(stdin) != '\n')
-              ;
-          return 0; // Invalid input
-      }
-
-      return grade;
-  }
+void clearInputBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
